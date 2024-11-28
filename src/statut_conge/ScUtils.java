6bf35120003d0719d692ecf430542d618447c6ee
hasinajy <jyhasina@gmail.com>
2024-11-28 22:19:40 +0300

    feat: Add hasFilledOneYear() to ScUtils

--- src/statut_conge/ScUtils.java
+++ src/statut_conge/ScUtils.java
@@ -1,22 +1,44 @@
 package statut_conge;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Period;
 
 import model.Employe;
 
 public class ScUtils {
 
-    public static boolean canTakeConge(Employe emp, Connection conn) {
+    public static boolean canTakeConge(Employe emp, Connection conn) throws SQLException {
         return hasFilledOneYear(emp, conn) && hasCongeLeft(emp, conn);
     }
 
-    private static boolean hasFilledOneYear(Employe emp, Connection conn) {
+    private static boolean hasFilledOneYear(Employe emp, Connection conn) throws SQLException {
         if (emp.getDateEmbauche() != null) {
-            // return NOW - date embauche
+            LocalDate hireDate = emp.getDateEmbauche().toLocalDate();
+            LocalDate currentDate = LocalDate.now();
+            Period period = Period.between(hireDate, currentDate);
+            return period.getYears() >= 1;
         }
 
-        // Get date embauche from database
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT date_embauche FROM Employe WHERE id_employe = ?")) {
+            stmt.setInt(1, emp.getIdEmploye());
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Date sqlHireDate = rs.getDate("date_embauche");
+
+                if (sqlHireDate != null) {
+                    LocalDate hireDate = sqlHireDate.toLocalDate();
+                    LocalDate currentDate = LocalDate.now();
+                    Period period = Period.between(hireDate, currentDate);
+                    return period.getYears() >= 1;
+                }
+            }
+        }
 
         return false;
     }
