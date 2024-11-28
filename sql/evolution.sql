8e5a7cae982ed47cd468a331370f34eafdcd4844
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-11-28 22:09:01 +0300

    add evolution for table education and diplome

---
+++ sql/evolution.sql
@@ -0,0 +1,5 @@
+ALTER TABLE education_candidat ADD COLUMN id_branche_education INTEGER;
+ALTER TABLE education_candidat ADD CONSTRAINT fkey_id_branche_education FOREIGN KEY(id_branche_education) REFERENCES branche_education(id_branche_education);
+
+ALTER TABLE diplome_requis_poste ADD COLUMN id_branche_education INTEGER;
+ALTER TABLE diplome_requis_poste ADD CONSTRAINT fkey_id_branche_education_diplome_poste FOREIGN KEY(id_branche_education) REFERENCES branche_education(id_branche_education);
