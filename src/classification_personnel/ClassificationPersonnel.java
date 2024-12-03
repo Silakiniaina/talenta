96ed2278ce189525d4b092db7da23b77184591e2
hasinajy <jyhasina@gmail.com>
2024-12-03 07:21:18 +0300

    feat: Create ClassificationPersonnel

--- src/classification_personnel/ClassificationPersonnel.java
+++ src/classification_personnel/ClassificationPersonnel.java
@@ -2,4 +2,43 @@ package classification_personnel;
 
 public class ClassificationPersonnel {
 
+    private String firstName;
+    private String lastName;
+    private String categorie;
+
+    /* ------------------------------ Constructors ------------------------------ */
+    public ClassificationPersonnel() {
+    }
+
+    public ClassificationPersonnel(String firstName, String lastName, String categorie) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setCategorie(categorie);
+    }
+
+    /* --------------------------- Getters and setters -------------------------- */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
 }
