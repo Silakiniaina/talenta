f948cc9088eced02110a8bda6dbf014b578f3a9a
hasinajy <jyhasina@gmail.com>
2024-12-03 07:37:41 +0300

    feat: Implement CpController

--- src/classification_personnelle/CpController.java
+++ src/classification_personnelle/CpController.java
@@ -1,24 +1,42 @@
 package classification_personnelle;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.utils.Database;
 
 public class CpController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            int categorieId = Integer.parseInt(req.getAttribute("categorie_id").toString());
+        int categorieId;
+        List<ClassificationPersonnelle> cpList = new ArrayList<>();
 
-            // Fetch the Cp by categorie_id
-            // Redirect to the view with the attribute cp
-        } catch (NumberFormatException nfe) {
-            // TODO: Log exception in here
+        // Services
+        ClassificationPersonnelle cp = new ClassificationPersonnelle();
+
+        try (Connection conn = Database.getConnection()) {
+            String categorieIdStr = req.getAttribute("categorie_id").toString();
+
+            try {
+                categorieId = Integer.parseInt(categorieIdStr);
+                cpList = cp.getByCategorie(categorieId, conn);
+            } catch (NumberFormatException nfe) {
+                cpList = cp.getAll(conn);
+            }
+        } catch (SQLException e) {
+            // Log exception here
         }
+
+        req.setAttribute("cp-list", cpList);
+        // TODO: Redirect to the view
     }
 
 }
