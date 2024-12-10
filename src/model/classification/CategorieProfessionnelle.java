e7fbc20d9ee9672c7741b09f1d870277323d6ed4
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-10 14:36:00 +0300

    creation model for Categorie Professionnelle

---
+++ src/model/classification/CategorieProfessionnelle.java
@@ -0,0 +1,22 @@
+package model.classification;
+
+public class CategorieProfessionnelle {
+    
+    private int idCategorieProfessionnelle;
+    private String nomCategorieProfessionnelle;
+
+
+    // GETTERS AND SETTERS
+    public int getIdCategorieProfessionnelle() {
+        return idCategorieProfessionnelle;
+    }
+    public String getNomCategorieProfessionnelle() {
+        return nomCategorieProfessionnelle;
+    }
+    public void setIdCategorieProfessionnelle(int idCategorieProfessionnelle) {
+        this.idCategorieProfessionnelle = idCategorieProfessionnelle;
+    }
+    public void setNomCategorieProfessionnelle(String nomCategorieProfessionnelle) {
+        this.nomCategorieProfessionnelle = nomCategorieProfessionnelle;
+    }
+}
