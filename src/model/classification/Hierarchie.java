b4b8b5235b98fc4f7117452f524f075a531863e6
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-10 11:28:41 +0300

    creation classe Hierarchie

---
+++ src/model/classification/Hierarchie.java
@@ -0,0 +1,24 @@
+package model.classification;
+
+public class Hierarchie {
+    
+    private int idHierarchie;
+    private String nomHierarchie;
+    
+    
+
+    // GETTERS
+    public int getIdHierarchie() {
+        return idHierarchie;
+    }
+    public String getNomHierarchie() {
+        return nomHierarchie;
+    }
+
+    public void setIdHierarchie(int idHierarchie) {
+        this.idHierarchie = idHierarchie;
+    }
+    public void setNomHierarchie(String nomHierarchie) {
+        this.nomHierarchie = nomHierarchie;
+    }
+}
