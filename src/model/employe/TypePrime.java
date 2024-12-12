f1ca452b51850b8c37d8f520774a91735d59c5d2
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-12 14:23:48 +0300

    model for type prime

---
+++ src/model/employe/TypePrime.java
@@ -0,0 +1,21 @@
+package model.employe;
+
+public class TypePrime {
+    
+    private int idTypePrime; 
+    private String nomTypePrime;
+
+    // GETTERS AND SETTERS
+    public int getIdTypePrime() {
+        return idTypePrime;
+    }
+    public String getNomTypePrime() {
+        return nomTypePrime;
+    }
+    public void setIdTypePrime(int idTypePrime) {
+        this.idTypePrime = idTypePrime;
+    }
+    public void setNomTypePrime(String nomTypePrime) {
+        this.nomTypePrime = nomTypePrime;
+    }
+}
