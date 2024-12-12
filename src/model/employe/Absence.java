64f711b5da8ed8d73b790d0d042a82c81c0e1294
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-12 13:48:49 +0300

    get all absence

--- src/model/employe/Absence.java
+++ src/model/employe/Absence.java
@@ -2,9 +2,14 @@ package model.employe;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Employe;
+import model.utils.Database;
 
 public class Absence {
     
@@ -13,6 +18,44 @@ public class Absence {
     private Date dateDebut; 
     private Date dateFin;
 
+
+    public List<Absence> getAll(Connection c) throws SQLException{
+        List<Absence> result = new ArrayList<>();
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null; 
+        ResultSet rs = null;
+        String query = "SELECT * FROM absence";
+        try {
+            if(c == null){
+                isNewConnection = true;
+                c = Database.getConnection();
+            }
+            prstm = c.prepareStatement(query);
+            rs = prstm.executeQuery();
+
+            while (rs.next()) {
+                Absence d = new Absence();
+                d.setIdAbsence(rs.getInt(1));
+                d.setEmploye(c, rs.getInt(2));
+                d.setDateDebut(rs.getDate(3));
+                d.setDateFin(rs.getDate(4));
+                result.add(d);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }finally{
+            if(rs != null){
+                rs.close();
+            }
+            if(prstm != null){
+                prstm.close();
+            }
+            if(c != null && isNewConnection){
+                c.close();
+            }
+        }
+        return result;
+    }
 
     public int getIdAbsence() {
         return idAbsence;
