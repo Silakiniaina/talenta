07d30ae8c4543f6627f19b80a7c36b2e6f32a867
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-12 11:54:31 +0300

    getAll for presence

--- src/model/employe/Presence.java
+++ src/model/employe/Presence.java
@@ -1,10 +1,18 @@
 package model.employe;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
 
 import model.Employe;
+import model.classification.Hierarchie;
+import model.utils.Database;
 
 public class Presence {
     
@@ -13,6 +21,43 @@ public class Presence {
     private Timestamp dateHeureEntree;
     private Timestamp dateHeureSortie;
 
+    public List<Presence> getAll(Connection c) throws SQLException{
+        List<Presence> result = new ArrayList<>();
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null; 
+        ResultSet rs = null;
+        String query = "SELECT * FROM presence_employe";
+        try {
+            if(c == null){
+                isNewConnection = true;
+                c = Database.getConnection();
+            }
+            prstm = c.prepareStatement(query);
+            rs = prstm.executeQuery();
+
+            while (rs.next()) {
+                Presence d = new Presence();
+                d.setIdPresence(rs.getInt(1));
+                d.setEmploye(c, rs.getInt(2));
+                d.setDateHeureEntree(rs.getTimestamp(3));
+                d.setDateHeureSortie(rs.getTimestamp(4));
+                result.add(d);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }finally{
+            if(rs != null){
+                rs.close();
+            }
+            if(prstm != null){
+                prstm.close();
+            }
+            if(c != null && isNewConnection){
+                c.close();
+            }
+        }
+        return result;
+    }
 
     // GETTERS AND SETTERS
     public int getIdPresence() {
@@ -40,4 +85,17 @@ public class Presence {
         this.dateHeureSortie = dateHeureSortie;
     }
 
+    public static void main(String[] args) {
+        try {
+            Connection c = Database.getConnection();
+
+            List<Presence> ls = new Presence().getAll(c);
+            Gson g = new Gson();
+            for(Presence p : ls){
+                System.out.println(g.toJson(p));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
