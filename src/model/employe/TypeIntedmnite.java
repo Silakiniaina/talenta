e6eb04e4b223882420107a7f036e7450e978c433
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-12-12 17:00:05 +0300

    model for type indemnite

---
+++ src/model/employe/TypeIntedmnite.java
@@ -0,0 +1,21 @@
+package model.employe;
+
+public class TypeIntedmnite{
+
+    private int idTypeIndemnite;
+    private String nomTypeIndemnite;
+
+    
+    public int getIdTypeIndemnite() {
+        return idTypeIndemnite;
+    }
+    public String getNomTypeIndemnite() {
+        return nomTypeIndemnite;
+    }
+    public void setIdTypeIndemnite(int idTypeIndemnite) {
+        this.idTypeIndemnite = idTypeIndemnite;
+    }
+    public void setNomTypeIndemnite(String nomTypeIndemnite) {
+        this.nomTypeIndemnite = nomTypeIndemnite;
+    }
+}
